285a3c518dff935200a19b4fa46e8ae698891dfa
Chancheep <51604377+NChancheep@users.noreply.github.com>
2022-04-04 21:21:26 +0700

    update

--- watpao/lib/src/cover.dart
+++ watpao/lib/src/cover.dart
@@ -4,16 +4,34 @@ class CoverPage extends StatelessWidget {
   const CoverPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Get started'),
-          onPressed: () {
-            Navigator.pushNamed(context, '/homePage');
-          },
-        ),
-      ),
-    );
-  }
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: Center(
+  //       // Image.asset('assets/watpao_cover.png'),
+  //       child: ElevatedButton(
+  //         child: const Text('Get started'),
+  //         onPressed: () {
+  //           Navigator.pushNamed(context, '/homePage');
+  //         },
+  //       ),
+  //     ),
+  //   );
+  // }
+  @override  
+  Widget build(BuildContext context) {  
+   
+    return MaterialApp(  
+        
+      home: Scaffold(     
+        body: Center(  
+          child: Column(              
+            children: <Widget>[  
+              Image.asset('assets/watpao_cover.png'),  
+            ], 
+            
+          ),
+        ),  
+      ),  
+    );  
+  }  
 }
